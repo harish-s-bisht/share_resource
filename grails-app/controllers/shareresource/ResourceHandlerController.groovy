4bd42c61d196e781b5b48eae7e74f8986abfc071
harish-s-bisht <harish@nexthoughts.com>
2017-11-28 17:25:12 +0530

    added new method for creating resource

--- grails-app/controllers/shareresource/ResourceHandlerController.groovy
+++ grails-app/controllers/shareresource/ResourceHandlerController.groovy
@@ -4,7 +4,15 @@ import org.springframework.security.access.annotation.Secured
 
 @Secured('permitAll')
 class ResourceHandlerController {
+    def resourceService
     @Secured('permitAll')
     def index() { }
+    @Secured('permitAll')
+    def create(Resource resource){
+        bindData(resource,params)
+        resourceService.create(resource)
+        render (view:"/user/userDashboard",model: [MSG:"Link Shared Successfully"])
+    }
+
 
 }
