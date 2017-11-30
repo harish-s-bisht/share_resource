476870e04862ccbf618d94aac9b2d79af01ebc3d
harish-s-bisht <harish@nexthoughts.com>
2017-11-30 19:27:05 +0530

    added some action

--- grails-app/controllers/shareresource/ResourceHandlerController.groovy
+++ grails-app/controllers/shareresource/ResourceHandlerController.groovy
@@ -1,5 +1,6 @@
 package shareresource
 
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.annotation.Secured
 
 @Secured('permitAll')
@@ -7,12 +8,17 @@ class ResourceHandlerController {
     def resourceService
     @Secured('permitAll')
     def index() { }
-    @Secured('permitAll')
-    def create(Resource resource){
-        bindData(resource,params)
-        resourceService.create(resource)
-        render (view:"/user/userDashboard",model: [MSG:"Link Shared Successfully"])
-    }
 
+    def createlink(Resource resource){
+        bindData(resource,params)
+        resourceService.createlink(resource)
+        redirect(controller: "user", action: "home", params: ["MSG":"Link Shared Successfully"])
+   }
+     def topic(Topic topic){
+         bindData(topic,params)
+         resourceService.topic(topic)
+         redirect(controller: "user", action: "home", params: ["MSG":"Topic Created Successfully"])
+
+     }
 
 }
--- grails-app/controllers/shareresource/UserController.groovy
+++ grails-app/controllers/shareresource/UserController.groovy
@@ -1,26 +1,37 @@
 package shareresource
 
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsHttpSession
+import org.springframework.http.HttpStatus
 import org.springframework.security.access.annotation.Secured
+import org.springframework.security.core.context.SecurityContext
 
 @Secured('permitAll')
 class UserController {
-
+    def springSecurityService
     def userService
     def index() { }
+
 
     def create(User user) {
         bindData(user, params,[exclude: ['pass']])
         user.@photoPath="/harish/"
+        if(user==null){
+            render status: HttpStatus.NOT_FOUND
+            return;
+        }
+        if(user.hasErrors()){
+            respond user.errors,view: "../target/work/plugins/spring-security-core-2.0.0/grails-app/views/login/auth.gsp"
+            return;
+        }
         userService.createUser(user)
-        render view: "../target/work/plugins/spring-security-core-2.0.0/grails-app/views/login/auth.gsp"
+        render (view: "../target/work/plugins/spring-security-core-2.0.0/grails-app/views/login/auth.gsp",model: [MSG:"User Account Created Successfully"])
 //        render view: "/resourceHandler/home"
     }
-//    @Secured('permitAll')
-    def dashboard(){
-        render view: "userDashboard"
-    }
+
     def home(){
-        List<Topic> topicList = userService.getAllTopics()
-        render (view:"userDashboard",model: [topicList: topicList])
+            List<Topic> topicList = userService.getAllTopics()
+            render(view: "userDashboard", model: [topicList: topicList,
+                                                  user:springSecurityService.currentUser],
+                                                    MSG: params.MSG)
     }
 }
